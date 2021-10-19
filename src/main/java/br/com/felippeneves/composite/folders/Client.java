00ed7daf4861f966f87e4676146a1aba9729f4db
Felippe Neves <felippenevesmonteiro@gmail.com>
2021-10-18 23:02:13 -0300

    Composite

---
+++ src/main/java/br/com/felippeneves/composite/folders/Client.java
@@ -0,0 +1,40 @@
+package br.com.felippeneves.composite.folders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.felippeneves.composite.folders.model.FileSystemItem;
+import br.com.felippeneves.composite.folders.model.Folder;
+import br.com.felippeneves.composite.folders.model.File;
+
+public class Client {
+
+	public static FileSystemItem createCompositeFromFile(java.io.File file) {
+		if(!file.isDirectory()) return new File(file.getName());
+		List<FileSystemItem> childs = new ArrayList<>();
+		java.io.File[] files = file.listFiles();
+		for(java.io.File javaFile: files) {
+			childs.add(createCompositeFromFile(javaFile));
+		}
+		return new Folder(file.getName(), childs);
+	}
+	
+	public static void main(String[] args) {
+		FileSystemItem file1 = new File("File1.txt");
+		FileSystemItem file2 = new File("File2.txt");
+		FileSystemItem file3 = new File("File3.txt");
+		FileSystemItem file4 = new File("File4.txt");
+		FileSystemItem sf1 = new Folder("sf1", Arrays.asList(file1));
+		FileSystemItem sf2 = new Folder("sf2", Arrays.asList(file2, sf1));
+		FileSystemItem folder = new Folder("folder", Arrays.asList(sf2, file3, file4));
+		
+		folder.print("");
+		
+		System.out.println("------------");
+		
+		String dir = System.getProperty("user.dir");
+		FileSystemItem fromDisk = createCompositeFromFile(new java.io.File(dir + java.io.File.separator + "src"));
+		fromDisk.print(">");
+	}
+}
---
+++ src/main/java/br/com/felippeneves/composite/folders/model/FileSystemItem.java
@@ -0,0 +1,5 @@
+package br.com.felippeneves.composite.folders.model;
+
+public interface FileSystemItem {
+	void print(String structure);
+}
